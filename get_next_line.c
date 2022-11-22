cd61fe5d3e7b92666d016a5ed9923732dbc0009e
Arthur <arthurascedusnkrs@gmail.com>
2022-11-22 11:01:21 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aascedu <aascedu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 09:13:59 by aascedu           #+#    #+#             */
-/*   Updated: 2022/11/22 10:41:14 by aascedu          ###   ########lyon.fr   */
+/*   Updated: 2022/11/22 11:01:16 by aascedu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 char	*get_next_line(int fd)
 {
 	static char	buff[BUFFER_SIZE + 1];
+	char		*line;
+	char		*temp;
 	int			length_read;
 
 	length_read = read(fd, buff, BUFFER_SIZE);
 	buff[BUFFER_SIZE + 1] = '\0';
 	if (length_read == BUFFER_SIZE && buff[BUFFER_SIZE] != '\n')
-	
+		temp = ft_strdup(buff);
+
 }
