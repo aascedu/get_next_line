363328cbdd5faccb3b913831ba9f1926b613dbd1
Arthur <arthurascedusnkrs@gmail.com>
2022-11-28 15:04:35 +0100

    RECOMMENCÉ

--- get_next_line.c
+++ get_next_line.c
@@ -5,29 +5,104 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aascedu <aascedu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/22 09:13:59 by aascedu           #+#    #+#             */
-/*   Updated: 2022/11/24 15:28:49 by aascedu          ###   ########lyon.fr   */
+/*   Created: 2022/11/28 11:05:04 by aascedu           #+#    #+#             */
+/*   Updated: 2022/11/28 15:04:02 by aascedu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-void	ft_bzero(void *s, size_t n)
+int	ft_strlen(char *str)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
-	if (n > 0)
-	{
-		while (i < n)
-		{
-			((char *)s)[i] = 0;
-			i++;
-		}
-	}
+	while (str[i])
+		i++;
+	return (i);
 }
 
+int	ft_newline(char *buff)
+{
+	int	i;
+
+	i = 0;
+	while (buff[i])
+	{
+		if (buff[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_index_nl(char *buff)
+{
+	int	i;
+
+	i = -1;
+	while (buff[++i])
+	{
+		if (buff[i] == '\n')
+		{
+			i++;
+			break;
+		}
+	}
+	return (i);
+}
+
+char	*ft_strjoin2(char *line, char *temp)
+{
+	char	*result;
+	int		line_len;
+	int		temp_len;
+	int		i;
+
+	line_len = ft_strlen(line);
+	temp_len = ft_strlen(temp);
+	result = (char *)malloc(sizeof(char) * (line_len + temp_len + 1));
+	if (!result)
+		return (NULL);
+	i = -1;
+	while (line[++i])
+		result[i] = line[i];
+	i = -1;
+	while (temp[++i])
+		result[line_len + i] = temp[i];
+	result[line_len + temp_len] = '\0';
+	free(line);
+	free(temp);
+	return (result);
+}
+
+char	*ft_strjoin(char *line, char *buff)
+{
+	char	*result;
+	int		line_len;
+	int		i;
+	int		j;
+
+	line_len = ft_strlen(line);
+	result = (char *)malloc(sizeof(char) * (line_len + BUFFER_SIZE + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (line[i])
+	{
+		result[i] = line[i];
+		i++;
+	}
+	j = 0;
+	while (buff[j])
+	{
+		result[i + j] = buff[j];
+		j++;
+	}
+	result[i + j] = '\0';
+	free(line);
+	return (result);
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -54,109 +129,80 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	return (dst);
 }
-/*
-char	*add_buffer(char *buff, char *result)
+
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (buff[i] && buff[i] != '\n')
-		i++;
-	if (buff[i] == '\n')
-		i++;
-	result = ft_strjoin(result, ft_strdup_index(buff, i));
-	ft_memmove(buff, buff + i, BUFFER_SIZE - i + 1);
-	ft_bzero(buff + BUFFER_SIZE - i, i);
-	return (result);
-}*/
-
-char	*add_buffer(char *result, char *buff, int size)
-{
-	int	i;
-
-	i = 0;
-	while (buff[i] && buff[i] != '\n')
-		i++;
-	if (buff[i] == '\n')
-		i++;
-	return (ft_strjoin(result, ft_strdup_index(buff, i), size));
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			((char *)s)[i] = 0;
+			i++;
+		}
+	}
 }
 
-
-/*char	*read_buffer(int fd, char *buff, char *result)
+char	*add_buffer(char *line, char *buff)
 {
-	int	i;
+	char	*temp;
+	int		i;
+	int		j;
 
-	i = 0;
-	if (length_read <= 0)
-	{
-		buff[BUFFER_SIZE] = EOF;
-		return (free(result), NULL);
-	}
-	while ( && !ft_is_set(buff, '\n'))
-	{
-		result = add_buffer(buff, result);
-		length_read = read(fd, buff, BUFFER_SIZE);
-	}
-	if (length_read < BUFFER_SIZE)
-	if (!ft_is_set(result, '\n'))
-		result = add_buffer(buff, result);
-	return (result);
-}*/
+	i = ft_index_nl(buff);
+	temp = (char *)malloc(sizeof(char) * (i + 1));
+	if (!temp)
+		return (NULL);
+	j = -1;
+	while (++j < i)
+		temp[j] = buff[j];
+	temp[j] = '\0';
+	temp = ft_strjoin2(line, temp);
+	ft_memmove(buff, buff + i, BUFFER_SIZE - i - 1);
+	ft_bzero(buff + (BUFFER_SIZE - i), BUFFER_SIZE);
+	return (temp);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	buff[BUFFER_SIZE + 1] = {0};
-	char		*result;
+	char		*line;
 	int			byte_read;
-	int			i;
 
-	if (read(fd, buff, 0) == -1)
+	line = (char *)malloc(sizeof(char));
+	if (!line)
 		return (NULL);
-	result = (char *)malloc(sizeof(char));
-	if (!result)
-		return (NULL);
-	*result = 0;
+	*line = 0;
 	byte_read = 1;
-	i = 1;
 	while (byte_read)
 	{
+		if (!buff[0])
+		{
+			byte_read = read(fd, buff, BUFFER_SIZE);
+		}
+		if (ft_newline(buff))
+			return (add_buffer(line, buff));
+		line = ft_strjoin(line, buff);
 		byte_read = read(fd, buff, BUFFER_SIZE);
-		if (!ft_is_set(buff, '\n'))
-		{
-			result = ft_strjoin(result, buff, i);
-		}
-		else
-		{
-			result = add_buffer(result, buff, i);
-			return (result);
-		}
-		i++;
 	}
-	return (result);
+	return (line);
 }
-
-/*
-
-
-#include <fcntl.h>
 
 int	main(void)
 {
-	int	fd = open("../../francinette/tests/get_next_line/gnlTester/files/alternate_line_nl_no_nl", O_RDONLY);
+	int	fd;
 
+	fd = open("../../francinette/tests/get_next_line/gnlTester/files/alternate_line_nl_with_nl", O_RDONLY);
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
-	close(fd);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	return (0);
 }
-*/
-/*
-int	main(void)
-{
-	char	*s;
-
-	s = ft_strdup_index("12", 2);
-	printf("%s\n", add_buffer(s, "bonjour\n"));
-}*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aascedu <aascedu@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 11:13:37 by aascedu           #+#    #+#             */
+/*   Updated: 2022/11/28 13:30:37 by aascedu          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
-# ifndef EOF
-#  define EOF -1
-# endif
 
-char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2, int size);
-char	*ft_strdup_index(char *s1, size_t index);
-
-int		ft_is_set(char *str, char c);
-int		ft_index_nl(char *line, int i);
-
-size_t	ft_strlen(char *str);
-
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
+//DELETE
+#include <stdio.h>
+#include <fcntl.h>
+//DELETE
 
 #endif
